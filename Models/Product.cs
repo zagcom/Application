18b11022adcfcc365a3d0097f20940f39e63f4fa
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-09-28 22:45:07 +0200

    Adding Product Model

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string EAN { get; set; }
+        [Required]
+        public Unit? Unit { get; set; }
+        public float Qty { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ Models/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public enum Unit
+    {
+        Each,
+        g,
+        Kg,
+        L,
+        mL
+    }
+}
