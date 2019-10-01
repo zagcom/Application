7ff151dbe0afeb10e87779dbb7a5a9ce45b440da
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-01 14:45:57 +0200

    Login Functionality

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -21,6 +21,38 @@ namespace Application.Controllers
             this.signInManager = signInManager;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "products");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "products");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
