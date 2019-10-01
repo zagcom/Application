e38e843d8b3b391e4773025f7c6392dea8dc2b1b
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-01 12:03:47 +0200

    Adding Identity

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Application.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,8 @@ namespace Application
             //applying dbcontext connection using dbContextPool (better performance) and the connetion string from appsettings.json 
             services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("ProductDBConnection")));
 
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
             services.AddMvc().AddXmlSerializerFormatters();
             services.AddScoped<IProductRepository, ProductRepository>();
         }
@@ -46,6 +49,7 @@ namespace Application
             }
 
             app.UseStaticFiles();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
