6a2df6aa811b8aacc163c2940fa48eb0d7a4582d
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-02 16:30:01 +0200

    Role Edit update

--- ViewModels/EditRoleViewModel.cs
+++ ViewModels/EditRoleViewModel.cs
@@ -8,6 +8,11 @@ namespace Application.ViewModels
 {
     public class EditRoleViewModel
     {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
         public string Id { get; set; }
         [Required(ErrorMessage = "Role Name is required")]
         public string RoleName { get; set; }
