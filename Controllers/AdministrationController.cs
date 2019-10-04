6e38d923b0dd8de78f6c3eaeb61877bf6f17f604
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-04 13:07:08 +0200

    Delete users

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -274,5 +274,33 @@ namespace Application.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListUsers");
+            }
+        }
+
     }
 }
