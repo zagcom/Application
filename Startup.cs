ce11e90268ec17eaa633baf386e8812c1f89bb92
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-06 23:41:51 +0200

    Multiple Handlers for Authorization

--- Startup.cs
+++ Startup.cs
@@ -64,6 +64,7 @@ namespace Application
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddSingleton<IAuthorizationHandler, CanEditOnlyOtherAdminRolesAndClaimsHandler>();
+            services.AddSingleton<IAuthorizationHandler, SuperAdminHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
