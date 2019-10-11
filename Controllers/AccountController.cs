f858e9734cae4a23c5e68110857fb3a271607f0d
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-11 22:24:25 +0200

    BudgetCategory Migration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -176,4 +176,4 @@ namespace Application.Controllers
     }
 
 }
-}
+
---
+++ Migrations/20191011200747_budgetCategory.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Application.Migrations
+{
+    public partial class budgetCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BudgetCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryLevel1Id = table.Column<int>(nullable: true),
+                    CategoryLevel2Id = table.Column<int>(nullable: true),
+                    CategoryLevel3Id = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BudgetCategories_Categories_CategoryLevel1Id",
+                        column: x => x.CategoryLevel1Id,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BudgetCategories_Categories_CategoryLevel2Id",
+                        column: x => x.CategoryLevel2Id,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BudgetCategories_Categories_CategoryLevel3Id",
+                        column: x => x.CategoryLevel3Id,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetCategories_CategoryLevel1Id",
+                table: "BudgetCategories",
+                column: "CategoryLevel1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetCategories_CategoryLevel2Id",
+                table: "BudgetCategories",
+                column: "CategoryLevel2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetCategories_CategoryLevel3Id",
+                table: "BudgetCategories",
+                column: "CategoryLevel3Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BudgetCategories");
+        }
+    }
+}
