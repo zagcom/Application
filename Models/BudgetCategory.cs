1ccceedabb43b6b48eae2b813f79be25b42650d0
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-11 14:42:41 +0200

    Budget Category model

---
+++ Models/BudgetCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class BudgetCategory
+    {
+        public int Id { get; set; }
+        public Category CategoryLevel1 { get; set; }
+        public Category CategoryLevel2 { get; set; }
+        public Category CategoryLevel3 { get; set; }
+        public string Name { get; set; }
+        public string OwnerId { get; set; }
+    }
+}
---
+++ Models/BudgetGroup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class BudgetGroup
+    {
+    }
+}
