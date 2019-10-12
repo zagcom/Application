5feb9ac71df5706611ec57c986b563281983815c
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-12 18:48:59 +0200

    Category Select List

--- Controllers/BudgetCategoriesController.cs
+++ Controllers/BudgetCategoriesController.cs
@@ -2,15 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Models;
+using Application.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Controllers
 {
     public class BudgetCategoriesController : Controller
     {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IBudgetCategoryRepository _budgetCategoryRepository;
+        private readonly IHostingEnvironment hostingEnvironment;
+        private readonly ILogger<BudgetCategoriesController> logger;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public BudgetCategoriesController(ICategoryRepository categoryRepository, IBudgetCategoryRepository budgetCategoryRepository, IHostingEnvironment hostingEnvironment, ILogger<BudgetCategoriesController> logger, UserManager<ApplicationUser> userManager)
+        {
+            _categoryRepository = categoryRepository;
+            _budgetCategoryRepository = budgetCategoryRepository;
+            this.hostingEnvironment = hostingEnvironment;
+            this.logger = logger;
+            this.userManager = userManager;
+        }
+
+        
+
         public IActionResult Index()
         {
+            var model = _budgetCategoryRepository.GetAllBudgetCategory();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var vm = new CreateBudgetCategoryViewModel();
+
+
+            var model = _categoryRepository.GetAllCategory();
+           
+            
+            foreach (var item in model)
+            {
+                if (item.CategoryLevel.Equals(0)){
+                    vm.CategoryLevel1List.Add(item);
+                }
+                if (item.CategoryLevel.Equals(1))
+                {
+                    vm.CategoryLevel2List.Add(item);
+                }
+                if (item.CategoryLevel.Equals(2))
+                {
+                    vm.CategoryLevel3List.Add(item);
+                }
+            }
+
+            
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Create(BudgetCategory model)
+        {
+            if (ModelState.IsValid)
+            {
+                BudgetCategory newCategory = new BudgetCategory
+                {
+                    Name = model.Name,
+                    CategoryLevel1 = model.CategoryLevel1,
+                    CategoryLevel2 = model.CategoryLevel2,
+                    CategoryLevel3 = model.CategoryLevel3,
+                    OwnerId = userManager.GetUserId(HttpContext.User)
+                };
+
+                _budgetCategoryRepository.Add(newCategory);
+                return RedirectToAction("details", new { id = newCategory.Id });
+            }
+
             return View();
+
         }
     }
 }
---
+++ ViewModels/CreateBudgetCategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Application.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class CreateBudgetCategoryViewModel
+    {
+        public List<Category> CategoryLevel1List { get; set; }
+        public List<Category> CategoryLevel2List { get; set; }
+        public List<Category> CategoryLevel3List { get; set; }
+
+      
+
+    }
+}
