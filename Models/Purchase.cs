e9d9fce943f8ef2104473555ec9b66600c149d4f
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-10-16 20:54:41 +0200

    Purchase model

---
+++ Models/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class Purchase
+    {
+        public string Id { get; set; }
+        public Product Product { get; set; }
+        public string ProductId { get; set; }
+        public BudgetCategory BudgetCategory { get; set; }
+        public int BudgetCategoryId { get; set; }
+        public float Qty { get; set; }
+        public float PricePerUnit { get; set; }
+        public float TotalPrice { get; set; }
+    }
+}
